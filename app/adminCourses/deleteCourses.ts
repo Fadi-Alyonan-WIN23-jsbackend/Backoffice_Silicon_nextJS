e9b3541b8a8e232d69c25ace137934abc995b479
SebastianWirstrom <143399640+SebastianWirstrom@users.noreply.github.com>
2024-05-23 13:18:33 +0200

    recommited deleteCourse

--- app/adminCourses/deleteCourses.ts
+++ app/adminCourses/deleteCourses.ts
@@ -3,7 +3,7 @@ async function deleteCourse(id: string): Promise<void> {
     mutation {
         deleteCourse(id: "${id}")
     }
-    `;
+    `
 
   const res = await fetch('https://coursesprovidergraphql.azurewebsites.net/api/GraphQL?code=9DnvhZulNJXNyVRryct85sroBFHfiY6TQw_iz4HRFfUvAzFuNCC0iA%3D%3D', {
     method: 'POST',
@@ -13,19 +13,19 @@ async function deleteCourse(id: string): Promise<void> {
       'Pragma': 'no-cache',
     },
     body: JSON.stringify({query: mutation}),
-  });
+  })
 
   if (!res.ok) {
-    throw new Error(`Could not delete course from CourseProvider: ${res.statusText}`);
+    throw new Error(`Could not delete course from CourseProvider: ${res.statusText}`)
   }
 
   const json = await res.json();
   if (json.errors) {
-    throw new Error(`Could not delete course: ${json.errors[0].message}`);
+    throw new Error(`Could not delete course: ${json.errors[0].message}`)
   }
 
-  console.log(`Deleted course with ID: ${id}`);
+  console.log(`Deleted course with ID: ${id}`)
 }
 
-export default deleteCourse;
+export default deleteCourse
 
