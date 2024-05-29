ea56017dc791449a9fa8928052653cf94757efa6
Fadi-Alyonan <fadialyonan@gmail.com>
2024-05-29 12:07:40 +0200

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
-export default nextConfig;
+ 
+module.exports = nextConfig
